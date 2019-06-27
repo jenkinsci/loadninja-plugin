173153a565bcb53be7c2077931d46639faf2fee6
Christopher Talavera <christopher.talavera@smartbear.com>
2019-06-27 12:11:32 -0400

    SMLD-2012 -jplugin- Removed a line of unused code to make the code cleaner

--- src/main/java/com/smartbear/loadninja/api/SimpleAPI.java
+++ src/main/java/com/smartbear/loadninja/api/SimpleAPI.java
@@ -4,7 +4,6 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
-import hudson.model.TaskListener;
 
 import com.smartbear.loadninja.helper.TestSummary;
 
